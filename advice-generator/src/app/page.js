312369666ad9da9ce51573cc4bd2730bc9748b82
AlfredoM90 <119818983+AlfredoM90@users.noreply.github.com>
2024-01-07 21:52:12 -0600

    quote of the day

---
+++ advice-generator/src/app/page.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://api.quotable.io/random");
+      const data = await res.json();
+      setQuote(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(quote);
+  return (
+    <div>
+      <h1>{quote.author}</h1>
+    </div>
+  );
+}
